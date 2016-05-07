fb78c2ab61e3d0bde519014267d7c1c0fd64a73e
Nico Hoffmann <on-github@nhoffmann.com>
2016-05-07 20:18:20 +0200

    Fixed thumb location

--- lib/thumb.php
+++ lib/thumb.php
@@ -11,8 +11,8 @@ class Thumb {
 
   public function __construct($url) {
     $this->url  = $url;
-    $this->dir  = kirby()->roots()->cache() . DS . 'oembed' . DS . 'thumbs';
-    
+    $this->dir  = kirby()->roots()->thumbs() . DS . '_plugins' . DS . 'oembed';
+
     $this->file = md5($this->url) . '.' . pathinfo($this->url, PATHINFO_EXTENSION);
     $this->root = $this->dir . DS . $this->file;
 
@@ -28,13 +28,13 @@ class Thumb {
 
   protected function cache() {
     if(!f::exists($this->root)) {
-      if(!file_exists($this->dir)) mkdir($this->dir);
+      if(!file_exists($this->dir)) mkdir($this->dir, 0777, true);
       file_put_contents($this->root, file_get_contents($this->url));
     }
   }
 
   public function __toString() {
-    return c::set('plugin.oembed.caching', true) ? url('thumbs/oembed/' . $this->file) : $this->url;
+    return c::set('plugin.oembed.caching', true) ? url('thumbs/_plugins/oembed/' . $this->file) : $this->url;
   }
 
 
